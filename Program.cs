6274f5ec71ee9906e475e3b84bb8f32edf67fdcb
Merrick Mount <339474587@GoTVDSB.ca>
2024-02-28 12:15:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace topic_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Soda();
+        }
+
+        public static void Soda()
+        {
+            Console.WriteLine("          .e$.                           z$$e.d$$$.      z$b   z\r\n         d$’ .d                        .$$’ d’F ^*$$$e  z$’ $ .$\r\n       e$P   $%                       d$P .’  F    ‘$$’d$  .e$’\r\n      $$F                           .$$’  F  J       ‘$$z$$$’\r\n    .$$’   .$’3   .$’’  .$P $$  $$ 4$$’  $  4’       $$  .\r\n   .$$F   d$  4  d$ d$ z$’ J$%    4$$’   $.d’       $$  .’\r\n   $$P   $$ ‘.$z$$  ^ z$’ .$P    .$$F              $$’ .’\r\n  $$$   d$F  J $$F   z$$  $$ .   $$$   ze     .c  J$F z  .e.ze\r\n 4$$F   $$  4’ $$   z$$  $$’.’  d$$  d$’ $  z$’ $ $$ @  $$’.$F\r\n $$$   4$$.'’ 4$$ .$3$$.$$$e%   $$P J$P  P d$*  %$$$’  $$  $$\r\n $$$    $$*    $$$’ ^$$’'$$    4$$% $$’ . d$’ ‘$’$$   $$  $$\r\n $$$                           $$$  ‘   P4$P  z 4$F  $$’ J$% %\r\n '$$c          .e$$$$$$$$e     $$$F    $ $$  z’ $$  $$$ 4$$ P\r\n  ‘$$b.   .e$*’     ‘$$$$$$$   '$$$c.dP  $$$$’  $$$’$$$$$$$P\r\n    ‘*$$$*’           ‘          *$$*’   ‘$*    ‘$’ ^$* ‘$’\r\n");
+        }
+    }
+}
